f9b6f9e71bb6f5226a70f460f941c6b7152353e8
besho0o <43782346+besho0o@users.noreply.github.com>
2018-10-17 07:07:40 +0300

    Update Project3.java
    Add Method

--- Project3.java
+++ Project3.java
@@ -15,10 +15,10 @@ public class Dijkstra {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
+        // Infinity variable equals 10000.
         final int INF = 10000;
-
+        // declare and initialize matrix
         int Matrix[][] = {{INF, 4, 3, INF, INF, INF},
                           {4, INF, 2, 13, 10, INF},
                           {3, 2, INF, 12, INF, INF},
@@ -30,13 +30,21 @@ public class Dijkstra {
         int[] distance = new int[6];
         int[] visited = new int[6];
         int[] preD = new int[6];
-        int min;
+        int min = 0;
         int nextnode = 0;
 
         distance = Matrix[0];
         distance[0] = 0;
         visited[0] = 1;
 
+        // Call the Dijkstra method,
+        Dijkstra(Matrix, min, INF, distance, visited, nextnode, preD);
+        // Matrix after running Dijkstra algorithm:
+        print(Matrix, preD);
+
+    }
+
+    public static void Dijkstra(int[][] Matrix, int min, int INF, int[] distance, int[] visited, int nextnode, int[] preD) {
         for (int i = 0; i < Matrix.length; i++) {
             min = INF;
             for (int j = 0; j < Matrix.length; j++) {
@@ -57,6 +65,10 @@ public class Dijkstra {
                 }
             }
         }
+
+    }
+
+    public static void print(int Matrix[][], int[] preD) {
         for (int i = 0; i < Matrix.length; i++) {
             int j;
             System.out.print("THE path is :" + i);
