2be85b439a2cff4b959cdc15e05bc8e006ee5f3f
avocado-toast99 <37838166+avocado-toast99@users.noreply.github.com>
2018-10-17 09:20:41 +0300

    added letters + path costs

--- Project3.java
+++ Project3.java
@@ -1,11 +1,11 @@
 /*
 
-Group Members:
+ Group Members:
 
 
-Algorithms and Data Structures
-Section CH 
-Project 3 - Dijkstra's Algorithm 
+ Algorithms and Data Structures
+ Section CH 
+ Project 3 - Dijkstra's Algorithm 
 
 
 
@@ -15,18 +15,17 @@ package dijkstra;
 
 public class Dijkstra {
 
-
     public static void main(String[] args) {
 
         // Infinity variable equals 10000.
         final int INF = 10000;
         // declare and initialize matrix
         int Matrix[][] = {{INF, 4, 3, INF, INF, INF},
-                          {4, INF, 2, 13, 10, INF},
-                          {3, 2, INF, 12, INF, INF},
-                          {INF, 13, 12, INF, 5, 1},
-                          {INF, 10, INF, 5, INF, 3},
-                          {INF, INF, INF, 1, 3, INF}};
+            {4, INF, 2, 13, 10, INF},
+            {3, 2, INF, 12, INF, INF},
+            {INF, 13, 12, INF, 5, 1},
+            {INF, 10, INF, 5, INF, 3},
+            {INF, INF, INF, 1, 3, INF}};
 
         // the row of matrix
         int[] distance = new int[6];
@@ -34,6 +33,8 @@ public class Dijkstra {
         int[] preD = new int[6];
         int min = 0;
         int nextnode = 0;
+        char[] nodes = {'a', 'b', 'c', 'd', 'e', 'f'};
+
 
         distance = Matrix[0];
         distance[0] = 0;
@@ -41,30 +42,31 @@ public class Dijkstra {
 
         // Call the Dijkstra method,
         Dijkstra(Matrix, min, INF, distance, visited, nextnode, preD);
+
         // Matrix after running Dijkstra algorithm:
-        print(Matrix, preD);
+        print(Matrix, preD, nodes, distance);
 
     }
 
     public static void Dijkstra(int[][] Matrix, int min, int INF, int[] distance, int[] visited, int nextnode, int[] preD) {
 
- /*
-        Dijkstra’s Algorithm
+        /*
+         Dijkstra’s Algorithm
         
         
-        - Description:
+         - Description:
         
         
-        - Parameters:
+         - Parameters:
         
         
         
-        - Output:
+         - Output:
         
         
 
-        */
-    
+         */
+
 
 
         for (int i = 0; i < Matrix.length; i++) {
@@ -87,20 +89,59 @@ public class Dijkstra {
                 }
             }
         }
-
     }
 
-    public static void print(int Matrix[][], int[] preD) {
+    public static void print(int Matrix[][], int[] preD, char[] nodes, int[] distance) {
         for (int i = 0; i < Matrix.length; i++) {
             int j;
-            System.out.print("THE shortest path from :"+i);
+            System.out.print("The shortest path from a to " + nodes[i] + ":   ");
+
             j = i;
 
+
+
             do {
-                j = preD[j];
-                System.out.print(" <- " + j);
+
+                if (distance[i] == 0) {
+                    System.out.print("NULL");
+                } else {
+
+
+                    do {
+                        j = preD[j];
+                        System.out.print(nodes[j] + " -> ");
+                    } while (j != 0);
+
+                    System.out.printf(nodes[i] + "\nwith the cost of " + distance[i] + "\n");
+
+                }
+
             } while (j != 0);
+
             System.out.println();
         }
     }
+//    public static void reverse(int[] preD) {
+//        
+//        System.out.println("BEFORE");
+//        for (int i = 0; i < preD.length; i++) {
+//            System.out.print(preD[i]);
+//            
+//        }
+//        
+//        
+//        for (int i = 0; i < preD.length/2; i++) {
+//            {
+//                int temp = preD[i];
+//                preD[i] = preD[preD.length - i - 1];
+//                preD[preD.length - i - 1] = temp;
+//            }
+//        }
+//        System.out.println("AFTER:");
+//
+//for (int i = 0; i < preD.length; i++) {
+//            System.out.print(preD[i]);
+//            
+//        }
+//    }
 }
